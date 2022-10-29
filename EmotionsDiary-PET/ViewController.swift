fa551c3b6bcbae5ecc6a0e5a7ae26120483dc812
marivolvakova <104025325+marivolvakova@users.noreply.github.com>
2022-10-29 13:44:01 +0300

    Initial Commit

---
+++ EmotionsDiary-PET/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionsDiary-PET
+//
+//  Created by Мария Вольвакова on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
